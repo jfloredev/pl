cef3d1eb2207ad2f4669bfe8f8dd20749fb01abc
jfloredev <juan.flores.p@utec.edu.pe>
2026-06-18 14:56:11 -0500

    fix : flopr , muxes

--- Pipelined.srcs/sources_1/imports/Src files/flopr.v
+++ Pipelined.srcs/sources_1/imports/Src files/flopr.v
@@ -1,13 +1,9 @@
-module flopr (input  clk, reset,
-               input  [WIDTH-1:0] d, 
-               output [WIDTH-1:0] q);
-
-  parameter WIDTH = 8;
-
-  reg [WIDTH-1:0] q; 
-
-  always @(posedge clk or posedge reset) begin 
-    if (reset) q <= 0; 
-    else       q <= d; 
-  end
+module flopr #(parameter WIDTH = 8) (
+  input              clk, reset,
+  input  [WIDTH-1:0] d,
+  output reg [WIDTH-1:0] q
+);
+  always @(posedge clk or posedge reset)
+    if (reset) q <= 0;
+    else       q <= d;
 endmodule
